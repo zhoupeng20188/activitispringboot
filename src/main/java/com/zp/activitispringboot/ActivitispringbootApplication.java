f7af364686d2d87e922ead331d3ca401929d918e
zhoupeng <287214764@qq.com>
2020-02-12 11:50:03 +0800

    Initial commit

---
+++ src/main/java/com/zp/activitispringboot/ActivitispringbootApplication.java
@@ -0,0 +1,13 @@
+package com.zp.activitispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitispringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitispringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zp/activitispringboot/ActivitispringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zp.activitispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitispringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
